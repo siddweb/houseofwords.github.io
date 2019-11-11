0b2c65b8f297e83478138c9b7656e66c2e965d3f
siddweb <55703715+siddweb@users.noreply.github.com>
2019-11-11 10:41:26 +0530

    Initial Commit

--- form-to-email.php
+++ form-to-email.php
@@ -32,7 +32,7 @@ $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
 //done. redirect to thank-you page.
-header('Location: index.html');
+header('URL: https://houseofwords.in');
 
 
 // Function to validate against any email injection attempts
